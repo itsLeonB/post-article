3300a7519a065624a812f20625dc0135b7afcbc4
itsLeonB <ellionblessan@gmail.com>
2024-08-17 22:03:21 +0700

    feat(frontend): add tabs on list page

--- frontend/src/app/routes/Listing/index.tsx
+++ frontend/src/app/routes/Listing/index.tsx
@@ -2,10 +2,13 @@ import { useEffect } from 'react';
 import { useAppDispatch } from '../../../hooks/useAppDispatch';
 import { useAppSelector } from '../../../hooks/useAppSelector';
 import { fetchPosts } from '../../../store/thunks/postListThunk';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 const Listing: React.FC = () => {
   const dispatch = useAppDispatch();
   const { data, status, error } = useAppSelector((state) => state.postList);
+  const statuses = ['publish', 'draft', 'thrash'];
 
   useEffect(() => {
     if (status === 'idle') {
@@ -23,32 +26,49 @@ const Listing: React.FC = () => {
 
   return (
     <main>
-      <table>
-        <thead>
-          <tr>
-            <td>
-              <h4>Title</h4>
-            </td>
-            <td>
-              <h4>Category</h4>
-            </td>
-            <td>
-              <h4>Status</h4>
-            </td>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((post) => {
-            return (
-              <tr key={post.title}>
-                <td>{post.category}</td>
-                <td>{post.category}</td>
-                <td>{post.status}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <Tabs>
+        <TabList>
+          <Tab>Published</Tab>
+          <Tab>Draft</Tab>
+          <Tab>Trashed</Tab>
+        </TabList>
+        {statuses.map((status) => {
+          return (
+            <TabPanel>
+              <table>
+                <thead>
+                  <tr>
+                    <td>
+                      <h4>Title</h4>
+                    </td>
+                    <td>
+                      <h4>Category</h4>
+                    </td>
+                    <td>
+                      <h4>Action</h4>
+                    </td>
+                  </tr>
+                </thead>
+                <tbody>
+                  {data
+                    .filter((item) => {
+                      return item.status === status;
+                    })
+                    .map((post) => {
+                      return (
+                        <tr key={post.title}>
+                          <td>{post.title}</td>
+                          <td>{post.category}</td>
+                          <td></td>
+                        </tr>
+                      );
+                    })}
+                </tbody>
+              </table>
+            </TabPanel>
+          );
+        })}
+      </Tabs>
     </main>
   );
 };
